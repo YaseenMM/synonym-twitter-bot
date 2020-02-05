985e9127677062b47ffbca2cb35ef8eab2e97b21
Yaseen Mohamed <ym1466@nyu.edu>
2020-02-05 16:10:07 -0500

    initial files/boiler plate

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: "78WRXGJs55bNnt00ZIhWviG9R",
+    consumer_secret: "9mxqfK2pSOo1VOautpakKDyREVmIXhlZHaPz62BZbZGIiM8h2H",
+    access_token: "893996444868386817-PbR3BXaMO15Ryc86NiJGaDM7FfmgLJ6",
+    access_token_secret: "IShCEIvwdQWaaqmi71iUH7HDQizOHsdvldh6Zan9Ty6Ib"
+}
